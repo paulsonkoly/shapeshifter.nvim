2a1b01144398b85d8581a17a89a67e3b18e20484
Paul Sonkoly <sonkoly.pal@gmail.com>
2023-11-08 19:25:43 +0000

    more specs

--- tests/shifters/curly_block_spec.lua
+++ tests/shifters/curly_block_spec.lua
@@ -11,7 +11,7 @@ describe("curly block shifter", function()
     buf_set_content(lines)
     vim.api.nvim_win_set_cursor(0, {1, 16})
 
-    it("rewrites the block to single line", function()
+    it("rewrites the block to be multi line", function()
       sh.shiftshapes()
 
       local result = vim.api.nvim_buf_get_lines(0, 0, -1, false)
@@ -20,6 +20,26 @@ describe("curly block shifter", function()
           "[1, 2, 3].each do |v|",
           "  puts v",
           "end"
+        }, result)
+    end)
+  end)
+
+  describe("with a comment at the end of line", function()
+    local lines = {
+      "[1, 2, 3].each { |v| puts v } # boo!"
+    }
+    buf_set_content(lines)
+    vim.api.nvim_win_set_cursor(0, {1, 16})
+
+    it("puts the comment after the block", function()
+      sh.shiftshapes()
+
+      local result = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+      assert.same(
+        {
+          "[1, 2, 3].each do |v|",
+          "  puts v",
+          "end # boo!"
         }, result)
     end)
   end)
